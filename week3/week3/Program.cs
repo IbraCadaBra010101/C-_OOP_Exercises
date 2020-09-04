5ae40fca61eab8e2220662caadd6da71cccd3e0a
ibraCadaBra010101 <ibma010101@gmail.com>
2020-09-04 09:04:09 +0100

    remove by index

--- week3/week3/Program.cs
+++ week3/week3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace week3
 {
@@ -6,7 +7,61 @@ namespace week3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region create a list
+
+            //List<int> nums = new List<int>();
+            // for (int i = 0; i < 5; i++)
+            // {
+            //     nums.Add(i);
+            // }
+
+            // foreach (var number in nums)
+            // {
+            //     Console.WriteLine("The following numbers are in the list: {0}", number);
+            // }
+            //numbersInput();
+            //List<string> colorList = new List<string>() { "red", "green", "white", "black", "pink", "yellow" };
+            //List<string> result = RemoveFromList(colorList);
+            //foreach (var s in result)
+            //{ 
+            //    Console.WriteLine(s);
+            //}
+            #endregion
+            #region AddNumbers
+
+            //static void numbersInput()
+            //{
+            //    List<int> nums = new List<int>() {1,2,3};
+
+            //    for (int i = 0; i < 2; i++)
+            //    {   Console.WriteLine("Which number would you like to add?");
+            //        int userInput= int.Parse(Console.ReadLine());
+            //        nums.Add(userInput);
+            //    }
+            //    Console.WriteLine("Numbers currently in list: ");
+            //    foreach (var num in nums)
+            //    {
+            //         Console.WriteLine(num);
+            //    }
+
+            //}
+            #endregion
+            #region RemovingByIndex
+
+            //static List<string> RemoveFromList(List<string> list)
+            //{
+            //    for (int i = list.Count - 1; i >= 0; i--)
+            //    {
+            //        if (i == 0 || i == 4 || i == 5)
+            //        {
+            //            list.RemoveAt(i);
+            //        }
+
+            //    }
+            //    return list;
+            //}
+
+            #endregion
         }
     }
 }
