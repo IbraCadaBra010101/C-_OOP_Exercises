17cb61a00d4738174a872253fe8186f83d7c0b5e
ibraCadaBra010101 <ibma010101@gmail.com>
2020-09-17 21:28:39 +0100

    switch cases in word

--- week4/week4/week4/Program.cs
+++ week4/week4/week4/Program.cs
@@ -8,8 +8,6 @@ namespace week4
     {
         static void Main(string[] args)
         {
-            CheckIfSubstringExists("text", "tests exists in the string");
-
         }
 
         #region Separate characters
@@ -27,7 +25,6 @@ namespace week4
         //    return wordsArray.Length;
         //}
         #endregion
-
         #region String contains substring
         static void CheckIfSubstringExists(string substring, string txt)
         {
@@ -40,8 +37,72 @@ namespace week4
                 Console.WriteLine($"{substring} does not exist in substring");
             }
         }
+        #endregion
+        #region switchCase
+
+        //private static string UserInput()
+        //{
+        //    var userInputWord = Console.ReadLine();
+        //    while (string.IsNullOrWhiteSpace(userInputWord) || int.TryParse(userInputWord, out var _))
+        //    {
+        //        Console.WriteLine("Error: enter a word"); 
+        //        userInputWord = Console.ReadLine();
+        //    }
+
+        //    return userInputWord;
+        //}
+
+        //private static string SwitchCases(string text)
+        //{
+        //    var switchedCases = "";
+        //    foreach (var character in text)
+        //    {
+        //        if (char.IsUpper(character))
+        //        {
+        //            switchedCases +=  char.ToLower(character);
+        //        }
+        //        else
+        //        {
+        //           switchedCases += char.ToUpper(character);
+        //        }
+        //    }
+        //    return switchedCases;
+        //}
+
+        //static string SwitchCases(string text)
+        //{
+        //    var switchedCases = "";
+        //    foreach (var character in text)
+        //    {
+        //        var asciiValue = (int) character;
+        //        if (asciiValue >= 97 && asciiValue <= 122) //check if char is lowercase
+        //        {
+        //            switchedCases += (char)(character - 32); //make it uppercase
+        //        }
+        //        else if (asciiValue >= 65 && asciiValue <= 90) //check if it's uppercase
+        //        {
+        //            switchedCases += (char)(character + 32); //make it lowercase
+        //        }
+        //        else
+        //        {
+        //            throw new Exception("Character is not a letter");
+        //        }
+        //    }
+        //    return switchedCases;
+        //}
+
+
+
+
+
+
+
+
+
+
 
         #endregion
     }
+
 
 }
